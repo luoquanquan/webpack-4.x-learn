83b7027c53ef9b8867d9ebd6595f785ffe7b490f
luoquanquan <luoquanquan@baidu.com>
2020-07-26 18:03:09 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,14 @@ module.exports = {
     filename: 'bundle.js',
     // 路径必须是绝对路径
     path: path.resolve(__dirname, 'dist')
+  },
+  // 开发服务配置
+  devServer: {
+    port: 3000,
+    progress: true,
+    // 静态文件服务托管的目录, 默认是当前目录
+    contentBase: './dist',
+    // gzip
+    compress: true
   }
 }
