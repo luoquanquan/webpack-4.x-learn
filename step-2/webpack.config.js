8470605e8713176c2c7f612e891390cf9381d486
luoquanquan <luo_quanquan@163.com>
2020-07-28 23:59:22 +0800

    copy-webpack-plugin

--- step-2/webpack.config.js
+++ step-2/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -39,6 +40,11 @@ module.exports = {
       template: './public/index.html',
       filename: 'home.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './docs', to: 'docs' }
+      ],
+    }),
   ]
 }
