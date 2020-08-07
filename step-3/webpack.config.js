62be4bab68e058b076c14846781843520e4b9264
luoquanquan <luoquanquan@baidu.com>
2020-08-07 22:52:27 +0800

    创建一个项目雏形

---
+++ step-3/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HWP = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HWP({
+      template: path.resolve(__dirname, './public/index.html')
+    })
+  ]
+}
