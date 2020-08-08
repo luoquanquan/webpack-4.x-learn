fc236122f67b8be7c17de23c5988d6243bbaaf71
luoquanquan <luoquanquan@baidu.com>
2020-08-08 09:17:03 +0800

    抽离公共代码

---
+++ step-3/dist/common~pageA~pageB.js
@@ -0,0 +1,25 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common~pageA~pageB"],{
+
+/***/ "./src/mA.js":
+/*!*******************!*\
+  !*** ./src/mA.js ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+console.log('mA ----');
+
+/***/ }),
+
+/***/ "./src/mB.js":
+/*!*******************!*\
+  !*** ./src/mB.js ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+console.log('mB ----');
+
+/***/ })
+
+}]);
--- step-3/webpack.config.js
+++ step-3/webpack.config.js
@@ -2,12 +2,26 @@ const path = require('path')
 const HWP = require('html-webpack-plugin')
 const webpack = require('webpack')
 const HappyPack = require('happypack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
+  devtool: false,
   entry: {
     pageA: './src/pageA.js',
     pageB: './src/pageB.js',
+  },
+  optimization: {
+    // 分割代码块
+    splitChunks: {
+      cacheGroups: {
+        common: {
+          minSize: 0,
+          minChunks: 2,
+          chunks: 'initial'
+        }
+      }
+    }
   },
   output: {
     filename: '[name].js',
@@ -43,6 +57,7 @@ module.exports = {
     new HWP({
       template: path.resolve(__dirname, './public/index.html')
     }),
-    new webpack.IgnorePlugin(/\.\/locale/, /moment/)
+    new webpack.IgnorePlugin(/\.\/locale/, /moment/),
+    new CleanWebpackPlugin()
   ]
 }
