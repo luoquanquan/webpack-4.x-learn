21813459f459f861da878351b1191e70aafea2a2
luoquanquan <luoquanquan@baidu.com>
2020-08-08 08:58:19 +0800

    如果使用了 commonjs 方法导入模块就不能删除 minus 方法的定义了

--- step-3/src/index.js
+++ step-3/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import calc from './calc'
+const calc = require('./calc').default
 
 ReactDOM.render(<h1>1 + 2 = {calc.sum(1, 2)}</h1>, document.getElementById('app'))
